b0b55e5ecca47a8d7f88681abf029eaeceb3613c
RicTieri <tieri.riccardo@gmail.com>
2023-11-24 15:37:34 +0100

    added print on page with easy design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const memberListContainer = document.getElementById('member_list');
+
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
 const memberList = [
@@ -8,33 +10,65 @@ const memberList = [
   {name:'Scott Estrada', role:'Developer', img_src:'scott-estrada-developer.jpg'},
   {name:'Barbara Ramos', role:'Founder & CEO', img_src:'barbara-ramos-graphic-designer.jpg'}
 ]
-// MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
-for (let index = 0; index < memberList.length; index++) {
-  const member = memberList[index];
-  console.log(member.name, member.role, member.img_src)
-}
-// MILESTONE 2:
-// Stampare le stesse informazioni su DOM sottoforma di stringhe
-const memberListContainer = document.getElementById('member_list');
+// // MILESTONE 1:
+// // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// for (let index = 0; index < memberList.length; index++) {
+//   const member = memberList[index];
+//   console.log(member.name, member.role, member.img_src)
+// }
+// // MILESTONE 2:
+// // Stampare le stesse informazioni su DOM sottoforma di stringhe
 
-for (let index = 0; index < memberList.length; index++) {
-  const member = memberList[index];
-  const textLine = document.createElement('p');
-  textLine.innerHTML = 'Name: ' + member.name + '; Role is: ' + member.role + '; Image directory is: ' + member.img_src;
-  memberListContainer.appendChild(textLine)
-}
-// BONUS 1:
-// Trasformare la stringa foto in una immagine effettiva
-for (let index = 0; index < memberList.length; index++) {
-  const member = memberList[index];
-  const textLine = document.createElement('p');
-  const imgEl = document.createElement('img');
-  imgEl.src = '../img/' + member.img_src;
-  textLine.innerHTML = 'Name: ' + member.name + '; Role is: ' + member.role;
-  memberListContainer.appendChild(textLine);
-  memberListContainer.appendChild(imgEl)
-}
+// for (let index = 0; index < memberList.length; index++) {
+//   const member = memberList[index];
+//   const textLine = document.createElement('p');
+//   textLine.innerHTML = 'Name: ' + member.name + '; Role is: ' + member.role + '; Image directory is: ' + member.img_src;
+//   memberListContainer.appendChild(textLine)
+// }
+// // BONUS 1:
+// // Trasformare la stringa foto in una immagine effettiva
+// for (let index = 0; index < memberList.length; index++) {
+//   const member = memberList[index];
+//   const textLine = document.createElement('p');
+//   const imgEl = document.createElement('img');
+//   imgEl.src = '../img/' + member.img_src;
+//   textLine.innerHTML = 'Name: ' + member.name + '; Role is: ' + member.role;
+//   memberListContainer.appendChild(textLine);
+//   memberListContainer.appendChild(imgEl)
+// }
 
 // BONUS 2:
 // Organizzare i singoli membri in card/schede, potete usare come esempio lo screenshot,  ma anche realizzare qualcosa di creativo!!!
+for (let index = 0; index < memberList.length; index++) {
+    const member = memberList[index]; 
+    memberListContainer.innerHTML += cardElGen(member.name, member.role, member.img_src);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function cardElGen(username, description, userphoto){
+  let gridItem =
+  `<div class="card m-2" style="width: 18rem;" >
+    <img src="../img/${userphoto}" class="card-img-top" alt="${username}">
+    <div class="card-body">
+      <h5 class="card-title">${username}</h5>
+      <p class="card-text">${description}</p>
+    </div>
+  </div>
+  `
+  return gridItem
+}
